b6609228ee6c48cc3ccc66e8e8986fbb0623df80
Aman Kumar <aman.it360@gmail.com>
2025-04-06 10:17:28 +0530

    Initial Commit

---
+++ BACKEND/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
