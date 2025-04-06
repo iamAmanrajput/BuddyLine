9bcb8ae5fbb7dc05639de1599380b89b2c2a4ff0
Aman Kumar <aman.it360@gmail.com>
2025-04-06 13:34:21 +0530

    Backend Completed

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import { Message, User } from "./interfaces";
 
 const app = express();
 app.use(cors());
@@ -13,4 +14,47 @@ const io = new Server(httpServer, {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
+});
+
+const users = new Map<string, User>();
+const messages: Message[] = [];
+
+io.on("connection", (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+
+  socket.on("join", (username: string) => {
+    users.set(socket.id, { id: socket.id, username: username });
+
+    io.emit("userList", Array.from(users.values()));
+    io.emit("userJoined", username);
+    io.emit("messageHistory", messages);
+  });
+
+  socket.on("sendMessage", (message: string) => {
+    const user = users.get(socket.id);
+    if (user) {
+      const msg: Message = {
+        user,
+        message,
+        timeStamp: new Date(),
+      };
+      messages.push(msg);
+      io.emit("newMessage", msg);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = users.get(socket.id);
+    if (user) {
+      console.log(`${user.username} left the chat`);
+      users.delete(socket.id);
+      io.emit("userList", Array.from(users.values()));
+      io.emit("userLeft", user.username);
+    }
+  });
+});
+
+const PORT = 8080;
+httpServer.listen(PORT, () => {
+  console.log(`Server is Running on port : ${PORT}`);
 });
